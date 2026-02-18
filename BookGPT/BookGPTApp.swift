d150cce32308e994376cc8c849be7e6bec616d45
maxidorov <s_maxim_v@bk.ru>
2026-02-18 17:47:09 +0200

    init project

---
+++ BookGPT/BookGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookGPTApp.swift
+//  BookGPT
+//
+//  Created by Maksim Sidorov on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
